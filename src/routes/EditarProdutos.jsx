8f8938d7976737424679e940a3cce8fbd39e009d
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-06 22:54:31 -0300

    Realizando destructuring + useState

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -23,6 +23,11 @@ export default function EditarProdutos() {
     preco: produtoFiltrado.preco,
   });
 
+  const handleChange = (event)=>{
+      const {name,value} = event.target;
+      setProduto({...produto, [name]:value});
+  }
+
   return (
     <>
       <h1>Editar Produtos</h1>
@@ -33,22 +38,23 @@ export default function EditarProdutos() {
             <legend>Produto Selecionado</legend>
             <div>
               <label htmlFor="idNome">Nome</label>
-              <input type="text" name="nome" id="idNome" value={produto.nome} />
+              <input type="text" name="nome" id="idNome" onChange={handleChange} value={produto.nome} />
             </div>
             <div>
               <label htmlFor="idDesc">Descrição</label>
-              <input type="text" name="desc" id="idDesc" value={produto.desc} />
+              <input type="text" name="desc" id="idDesc" onChange={handleChange} value={produto.desc} />
             </div>
             <div>
               <label htmlFor="idImg">Imagem</label>
-              <input type="url" name="img" id="idImg" value={produto.img} />
+              <input type="url" name="img" id="idImg" onChange={handleChange} value={produto.img} />
             </div>
             <div>
               <label htmlFor="idPreco">Preço</label>
               <input
                 type="text"
                 name="preco"
-                id="idPreco"
+                id="idPreco" 
+                onChange={handleChange} 
                 value={produto.preco}
               />
             </div>
