5ad0fe7123c26642cfa741f526e1dd63205ce957
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-06 22:04:14 -0300

    Recuperando o objeto produto com filter.

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,17 +1,23 @@
 import { useParams } from "react-router-dom";
+import { ListaProdutos } from "../components/ListaProdutos";
+
 
 export default function EditarProdutos() {
 
   document.title = "Editar Produtos";
 
   //Utilizando o HOOK useParams()
-
   const {id} = useParams();
+
+  //Filtrando o produto selecionado byId
+  const produtoFiltrado = ListaProdutos.filter( (item)=> item.id === parseInt(id) );
 
   return (
     <>
       <h1>EditarProdutos</h1>
       <p>Produto selecionado : {id}</p>
+      <p>Nome do produto : {produtoFiltrado[0].nome}</p>
+      <img src={produtoFiltrado[0].img} alt={produtoFiltrado[0].nome}/>
     </>
   )
 }
