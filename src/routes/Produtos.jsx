76448d6658cadcea61a1a7771ac85f2cda01025a
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-13 21:58:10 -0300

    Iniciando com CSS.MODULE, exemplo em tabela.

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,26 +1,20 @@
 import { Link } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
-import {AiFillEdit as Editar} from "react-icons/ai";
-import {RiDeleteBin2Fill as Excluir} from "react-icons/ri";
+import { AiFillEdit as Editar } from "react-icons/ai";
+import { RiDeleteBin2Fill as Excluir } from "react-icons/ri";
+import styles from "./Produtos.module.css";
 
 export default function Produtos() {
-
   document.title = "Produtos";
-
-  const estiloTabela = {
-    borderCollapse: "collapse",
-    margin:"0 auto",
-    border: "2px solid #ccc",
-  }
-
 
   return (
     <>
       <h1>Lista de Produtos</h1>
 
       <div>
+        <table className={styles.tblEstilo}>
 
-          <table style={estiloTabela}>
+          <thead className={styles.tblHeader}>
             <tr>
               <th>ID</th>
               <th>NOME</th>
@@ -29,23 +23,42 @@ export default function Produtos() {
               <th>PREÇO</th>
               <th>EDITAR/EXCLUIR</th>
             </tr>
-            
-              {ListaProdutos.map( (item,indice)=>(
-                  <tr key={indice}>
-                    <td>{item.id}</td>
-                    <td>{item.nome}</td>
-                    <td>{item.desc}</td>
-                    <td><img src={`${item.img}`} alt={`${item.desc}`} /></td>
-                    <td>{item.preco}</td>
-                    <td> <Link to={`/editar/produtos/${item.id}`}> <Editar/> </Link> / <Link to={`/excluir/produtos/${item.id}`}> <Excluir/> </Link></td>
-                  </tr>
-              ))}
+          </thead>
 
-          </table>
+          <tbody>
+            {ListaProdutos.map((item, indice) => (
+              <tr key={indice} className={styles.tblRow}>
+                <td>{item.id}</td>
+                <td>{item.nome}</td>
+                <td>{item.desc}</td>
+                <td>
+                  <img src={`${item.img}`} alt={`${item.desc}`} />
+                </td>
+                <td>{item.preco}</td>
+                <td>
+                  {" "}
+                  <Link to={`/editar/produtos/${item.id}`}>
+                    {" "}
+                    <Editar />{" "}
+                  </Link>{" "}
+                  /{" "}
+                  <Link to={`/excluir/produtos/${item.id}`}>
+                    {" "}
+                    <Excluir />{" "}
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
 
+          <tfoot>
+            <tr>
+              <td colSpan={6}>PRODUTOS</td>
+            </tr>
+          </tfoot>
+
+        </table>
       </div>
-
     </>
-  )
+  );
 }
- 
